8de95f59723ed9b01f4b58f340ed53073556804e
jasti.ohanna <jasti.ohanna@krom.id>
2024-10-17 13:23:34 +0700

    feat: initialize schdn ui

---
+++ src/app/login/(overview)/page.tsx
@@ -0,0 +1,10 @@
+// import AcmeLogo from '@/app/ui/acme-logo';
+import {LoginForm} from '@/lib/ui/login/login-form';
+
+export default function LoginPage() {
+  return (
+    <main className="flex items-center justify-center h-screen">
+        <LoginForm />
+    </main>
+  );
+}
